fdcdd9684428560305150f35aaa8123d3f05ead4
Byungju Kim <74722197+sh596@users.noreply.github.com>
2022-11-03 07:02:05 +0900

    feat : All page UI

---
+++ app/src/main/java/com/example/dudeulimproject/InterViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dudeulimproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InterViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inter_view)
+    }
+}
--- app/src/main/java/com/example/dudeulimproject/SplashActivity.kt
+++ app/src/main/java/com/example/dudeulimproject/SplashActivity.kt
@@ -2,10 +2,19 @@ package com.example.dudeulimproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dudeulimproject.base.BaseActivity
+import com.example.dudeulimproject.databinding.ActivitySplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+        }
     }
 }
